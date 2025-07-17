95284ab93cf4a31c266b6edf20b5db0040c61b09
Alic-Li <2591709191@qq.com>
2025-07-17 23:42:43 +0800

    Updata README&infer_script

---
+++ API_DEMO_CHAT.py
@@ -0,0 +1,140 @@
+########################################################################################################
+# The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
+########################################################################################################
+
+print("RWKV Chat Simple Demo")
+
+import os, copy, types, gc, sys, re
+import numpy as np
+from prompt_toolkit import prompt
+import torch
+from transformers import AutoTokenizer
+
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cuda.matmul.allow_tf32 = True
+os.environ["RWKV_V7_ON"] = "1" # enable this for rwkv-7 models
+os.environ["RWKV_JIT_ON"] = "1"
+os.environ["RWKV_CUDA_ON"] = "0"  # !!! '1' to compile CUDA kernel (10x faster), requires c++ compiler & cuda libraries !!!
+
+from rwkv.model import RWKV
+from rwkv.utils import PIPELINE
+
+########################################################################################################
+
+args = types.SimpleNamespace()
+
+args.strategy = "cuda fp16"  # use CUDA, fp16
+
+args.MODEL_NAME = "/home/alic-li/rwkv7-g1-tanslate/rwkv-final-sft-512"
+
+
+########################################################################################################
+STATE_NAME = None # use vanilla zero initial state?
+
+# use custom state? much better chat results (download from https://huggingface.co/BlinkDL/temp-latest-training-models/tree/main)
+# note: this is English Single-round QA state (will forget what you previously say)
+# STATE_NAME = "E://RWKV-Runner//models//rwkv-x060-eng_single_round_qa-1B6-20240516-ctx2048"
+########################################################################################################
+
+GEN_TEMP = 1.0
+GEN_TOP_P = 0.3
+GEN_alpha_presence = 0.5
+GEN_alpha_frequency = 0.5
+GEN_penalty_decay = 0.996
+
+if STATE_NAME != None:
+    GEN_TOP_P = 0.2
+    GEN_alpha_presence = 0.3
+    GEN_alpha_frequency = 0.3
+
+CHUNK_LEN = 16  # split input into chunks to save VRAM (shorter -> slower, but saves VRAM)
+
+########################################################################################################
+
+print(f"Loading model - {args.MODEL_NAME}")
+model = RWKV(model=args.MODEL_NAME, strategy=args.strategy)
+pipeline = PIPELINE(model, "rwkv_vocab_v20230424")
+tokenizer = AutoTokenizer.from_pretrained("/home/alic-li/minimind/MiniMind2/")
+
+model_tokens = []
+model_state = None
+
+if STATE_NAME != None: # load custom state
+    args = model.args
+    state_raw = torch.load(STATE_NAME + '.pth')
+    state_init = [None for i in range(args.n_layer * 3)]
+    for i in range(args.n_layer):
+        dd = model.strategy[i]
+        dev = dd.device
+        atype = dd.atype    
+        state_init[i*3+0] = torch.zeros(args.n_embd, dtype=atype, requires_grad=False, device=dev).contiguous()
+        state_init[i*3+1] = state_raw[f'blocks.{i}.att.time_state'].transpose(1,2).to(dtype=torch.float, device=dev).requires_grad_(False).contiguous()
+        state_init[i*3+2] = torch.zeros(args.n_embd, dtype=atype, requires_grad=False, device=dev).contiguous()
+    model_state = copy.deepcopy(state_init)
+
+def run_rnn(ctx):
+    global model_tokens, model_state
+
+    ctx = ctx.replace("\r\n", "\n")
+
+    tokens = tokenizer.encode(ctx)
+    tokens = [int(x) for x in tokens]
+    model_tokens += tokens
+
+    # print(f"### model ###\n{model_tokens}\n[{pipeline.decode(model_tokens)}]")  # debug
+
+    while len(tokens) > 0:
+        out, model_state = model.forward(tokens[:CHUNK_LEN], model_state)
+        tokens = tokens[CHUNK_LEN:]
+
+    return out
+
+if STATE_NAME == None: # use initial prompt if we are not loading a state
+    init_ctx = "User: hi" + "\n\n"
+    init_ctx += "Assistant: Hi. I am your assistant and I will provide expert full response in full details. Please feel free to ask any question and I will always answer it." + "\n\n"
+    # run_rnn(init_ctx)
+    # print(init_ctx, end="")
+
+while True:
+    msg = prompt("<|im_start|>user:")
+    msg = msg.strip()
+    msg = re.sub(r"\n+", "\n", msg)
+    if len(msg) > 0:
+        occurrence = {}
+        out_tokens = []
+        out_last = 0
+
+        out = run_rnn("<|im_start|>user\n" + msg + "<|im_end|>\n" + "<|im_start|>assistant\n")
+        print("\nAssistant:", end="")
+
+        eos_token_id = tokenizer.eos_token_id
+        pad_token_id = tokenizer.pad_token_id
+
+        for i in range(99999):
+            for n in occurrence:
+                out[n] -= GEN_alpha_presence + occurrence[n] * GEN_alpha_frequency  # repetition penalty
+            out[0] -= 1e10  # disable END_OF_TEXT
+
+            token = pipeline.sample_logits(out, temperature=GEN_TEMP, top_p=GEN_TOP_P)
+
+            out, model_state = model.forward([token], model_state)
+            model_tokens += [token]
+
+            out_tokens += [token]
+
+            for xxx in occurrence:
+                occurrence[xxx] *= GEN_penalty_decay
+            occurrence[token] = 1 + (occurrence[token] if token in occurrence else 0)
+
+            tmp = tokenizer.decode(out_tokens[out_last:])
+            if ("\ufffd" not in tmp) and (not tmp.endswith("\n")):
+                print(tmp, end="", flush=True)
+                out_last = i + 1
+
+            # 使用 token_id 判断是否为 eos_token
+            if token == eos_token_id:
+                print(tmp, end="\n\n", flush=True)
+                break
+    else:
+        print("!!! Error: please say something !!!")
--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# Mimi_RWKV_7
-Mini_RWKV_V7_LM
+# 🪿 Mini-RWKV-V7-LM 
+🚀 让我们来从头训练一个属于自己的Mini-RWKV-7吧~ 小小的鹅也能飞得很高喔~
+
+<div align="center">
+  <img src="./miniGoose.png" width="200" height="200" style="display: block; margin: auto;">
+</div>
+
+## 🌟 模型简介
+
+本模型是基于 **RWKV-V7 架构** 训练的一个 **34M 参数量** 的语言模型`Mini-RWKV-V7-LM-34M`。它在保持轻量的同时，具备良好的语言理解和生成能力，非常适合资源极其有限的设备部署和快速迭代开发。
+
+---
+
+## 📦 模型结构
+
+| 参数 | 数值 |
+|------|------|
+| 参数量 | 34.2M 🎯 |
+| 层数 | 8 🧱 |
+| 隐藏维度 | 512 📐 |
+| 上下文长度 | 512->1024->2048 📏 |
+| 词表大小 | 6400 📚 |
+- Vocab 和MiniMind的保持一致
+---
+
+## 🧪 训练信息
+
+- 🪿 架构：[RWKV-V7](https://github.com/BlinkDL/RWKV-LM) 
+- 📚 数据源：[minimind_dataset](https://huggingface.co/datasets/jingyaogong/minimind_dataset) 特别感谢MiniMind的作者 [@jingyaogong](https://github.com/jingyaogong)开源了训练数据集 🤗
+- 📈 学习率：动态调整  
+- 🖥️ 硬件：可以使用4060laptop等显卡进行训练，甚至Radeon 780M 核显也可以在轻薄本上进行训练 🚀  
+- 👀我是在AMD Instinct MI300X 上快速复现的(十分感谢AMD公司的对我个人以及RWKV的云算力赞助)
+- 📦 模型大小：68.4MB 参数量 34.2M Params
+- 📊 损失曲线：训练收敛稳定 loss < 2.0  
+
+---
+## 📚 支持任务
+
+- 📝 预训练（Pre-training）
+- 📚 监督微调训练（Supervised fine-tuning [SFT]）
+---
+
+## 🧰 推理方法
+
+### 🐍 安装依赖
+
+```bash
+pip install -r requirements.txt
+```
+- 如果你使用的是AMD Instinct MI300X，请不要安装requirements.txt中的torch，请安装对应最新版本的torch
+- 比如说```pip3 install torch --index-url https://download.pytorch.org/whl/rocm6.3```
+
+
+### 🧪 加载模型 & 推理示例
+
+```bash
+python3 ./API_DEMO_CHAT.py
+```
+
+## 🚀开始训练
+
+## 🪿学习率建议(LR)
+
+## 📢 使用须知
+
+
+## 📢 致谢
+
+- 🙌 感谢 RWKV 社区提供的开源代码和训练框架！
+- 💡 感谢所有参与语料清洗和训练的同学和朋友！
+- 🚀 感谢 [MiniMind](https://github.com/jingyaogong/minimind) 提供的 README 模板灵感！
+- 如发现 bug 或有任何建议，欢迎提交 issue 或 PR 🛠️
+
+
+---
+
+## 🧩 相关项目推荐
+
+- [MiniMind](https://github.com/jingyaogong/minimind)：一个轻量级 LLM 教学项目 📚
+- [RWKV-V7](https://github.com/BlinkDL/RWKV-LM)：RWKV 最新版本架构仓库 🧠
+- [RWKV-Runner](https://github.com/wangyuxinwhy/rwkv-runner)：便捷推理框架 🚀
+
+---
+
+🎉 感谢你使用 **RWKV-V7-LM-34M**！如果你喜欢这个项目，欢迎点赞、分享、Star 和 Follow！🌟
+
+--- 
